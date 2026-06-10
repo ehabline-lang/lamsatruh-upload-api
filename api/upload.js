d2a2eb7b982c29d7974cabe4d484f6744be33543
ehabline-lang <ehabline@gmail.com>
2026-06-10 10:48:55 +0300

    Update upload.js

--- api/upload.js
+++ api/upload.js
@@ -42,7 +42,7 @@ export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({
   success: false,
-  message: "Method not allowed",
+  message: "Method not allowed ehab",
   version: "formidable-v3-debug"
 });
 
